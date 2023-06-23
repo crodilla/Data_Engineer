5c1281dccc1cda4347198512fd7d64be1b2e79ac
crodilla <cristinarodillahernandez@gmail.com>
2023-06-23 20:59:58 +0200

    k

---
+++ Flask/Teoria/App/app.py
@@ -0,0 +1,4 @@
+from flask import Flask
+
+app = Flask(__name__) # Para crear dicha instancia, debemos pasar como
+
---
+++ Flask/Teoria/App/config.py
@@ -0,0 +1,8 @@
+DEBUG = True
+FLASK_APP = 'app.py'
+
+from app import a
+
+print ('estoy en config')
+print(a)
+print(__name__)
